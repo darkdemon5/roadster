c60f7b0ecd2507e8d568bc1298c2be50c9d69b0e
darkdemon5 <yash.sag55@gmail.com>
2024-07-04 15:29:17 +0530

    modified:   package-lock.json modified:   package.json new file:   src/components/HeroSection.jsx new file:   src/components/NavMobile.jsx modified:   src/components/Navbar.jsx new file:   src/routes/NavRoutes.jsx modified:   yarn.lock

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import { GrCircleQuestion } from "react-icons/gr";
 import { CgProfile } from "react-icons/cg";
 import { PiGlobe } from "react-icons/pi";
 import logo from "../assets/tesla.svg";
+import { routes } from "../routes/NavRoutes";
 
 const Navbar = () => {
+  // const [isOpen, setOpen] = useState(false);
   return (
     // <div className="mx-auto px-4 py-2 lg:px-8 lg:py-4">
     //   {/* <ul className="mx-auto flex items-center justify-between text-blue-gray-900"> */}
@@ -38,7 +40,7 @@ const Navbar = () => {
     //   {/* </ul> */}
     // </div>
 
-    <div className="grid grid-cols-3 flex">
+    <div className="mx-auto flex justify-between items-center grid grid-cols-3 flex">
       <div className="float-left ml-6 my-3">
         <a href="https://www.tesla.com/roadster">
           <img src={logo} alt="Tesla" className="w-[160px] p-4" />
@@ -46,11 +48,23 @@ const Navbar = () => {
       </div>
       <div className="content-center">
         <ul className="flex items-center justify-center ">
-          <li className="px-4">Vehicles</li>
+          {routes.map((route) => {
+            const { title, href } = route;
+            return (
+              <li className="px-4">
+                <a
+                  href={href}
+                  className="flex items-center gap-1 hover:text-neutral-400 transition-all"
+                ></a>
+                {title}
+              </li>
+            );
+          })}
+          {/* <li className="px-4">Vehicles</li>
           <li className="px-4">Energy</li>
           <li className="px-4">Charging</li>
           <li className="px-4">Discover</li>
-          <li className="px-4">Shop</li>
+          <li className="px-4">Shop</li> */}
         </ul>
       </div>
       <div className="content-center ">
---
+++ src/routes/NavRoutes.jsx
@@ -0,0 +1,32 @@
+import { BiHomeAlt2 } from "react-icons/bi";
+import { FiSearch } from "react-icons/fi";
+import { PiChatCircleBold } from "react-icons/pi";
+import { IoPricetagsOutline } from "react-icons/io5";
+
+export const routes = [
+  {
+    title: "Vehicles",
+    href: "#",
+    // Icon: BiHomeAlt2,
+  },
+  {
+    title: "Energy",
+    href: "#",
+    // Icon: FiSearch,
+  },
+  {
+    title: "Charging",
+    href: "#",
+    // Icon: IoPricetagsOutline,
+  },
+  {
+    title: "Discovery",
+    href: "#",
+    // Icon: PiChatCircleBold,
+  },
+  {
+    title: "Shop",
+    href: "#",
+    // Icon: PiChatCircleBold,
+  },
+];
